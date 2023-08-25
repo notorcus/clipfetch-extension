0e8c0e3f9f11d6a21d563f413aef5cc968189aa4
Akki <akshat.kr12090@gmail.com>
2023-08-25 12:58:54 -0400

    video downloads on the path correctly
    Now we're using youtube-dl-exec to download video on the specified path. Format hasn't been set yet

--- src/videoDownloader.js
+++ src/videoDownloader.js
@@ -1,22 +1,34 @@
-const ytdlpWrap = require('yt-dlp-wrap');
+// videoDownloader.js
+const youtubedl = require('youtube-dl-exec');
+const path = require('path');
 
 const downloadVideo = (youtubeLink) => {
     const cs = new CSInterface;
 
     // Set the output path
-    const outputPath = "C:\\Users\\Akshat Kumar\\Editing\\Media";
-
+    const outputPath = "C:\\Users\\Akshat Kumar\\Editing\\Media\\PProClipFetch";
     const videoPath = path.join(outputPath, 'downloadedVideo.mp4'); // This is where the video will be saved
 
     alert('Started download for: ' + youtubeLink); // Alert after starting the download
 
-    ytdlpWrap.exec([youtubeLink, '-o', videoPath], (err, data) => {
-        if (err) {
-            console.error('Error during download:', err);
-            alert('Error downloading video. Please try again.');
-            return;
-        }
-
+    youtubedl(youtubeLink, {
+        'output': videoPath,
+        'format': 'bestvideo+bestaudio/best',  // Download best video and audio format available
+        'no-check-certificate': true,
+        'no-warnings': true,
+        'prefer-free-formats': true,
+        'add-header': [
+            'referer:youtube.com',
+            'user-agent:googlebot'
+        ]
+    }).then(output => {
+        alert('Download complete!');
         cs.evalScript(`$.runScript.importVideo("${videoPath.replace(/\\/g, '\\\\')}")`);
+    }).catch(error => {
+        console.error('Error during download:', error);
+        alert('Error downloading video. Please try again.');
     });
 }
+
+
+window.downloadVideo = downloadVideo;
