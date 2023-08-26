6f0d83b598ee2d63308f025db714640e76da1080
Akki <akshat.kr12090@gmail.com>
2023-08-26 11:29:53 -0400

    convert video to mp4 using yt dl

--- src/videoDownloader.js
+++ src/videoDownloader.js
@@ -2,33 +2,36 @@
 const youtubedl = require('youtube-dl-exec');
 const path = require('path');
 
-const downloadVideo = (youtubeLink) => {
+const downloadAndConvertVideo = async (youtubeLink) => {
     const cs = new CSInterface;
 
     // Set the output path
     const outputPath = "C:\\Users\\Akshat Kumar\\Editing\\Media\\PProClipFetch";
-    const videoPath = path.join(outputPath, 'downloadedVideo.mp4'); // This is where the video will be saved
+    const videoPath = path.join(outputPath, 'convertedVideo'); // This is where the video will be saved
 
-    alert('Started download for: ' + youtubeLink); // Alert after starting the download
+    alert('Started download and conversion for: ' + youtubeLink); // Alert after starting the download
 
-    youtubedl(youtubeLink, {
-        'output': videoPath,
-        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]',  // Fetch best MP4 video and M4A audio
-        'no-check-certificate': true,
-        'no-warnings': true,
-        'prefer-free-formats': true,
-        'add-header': [
-            'referer:youtube.com',
-            'user-agent:googlebot'
-        ]
-    }).then(output => {
-        alert('Download complete!');
-        cs.evalScript(`$.runScript.importVideo("${videoPath.replace(/\\/g, '\\\\')}")`);
-    }).catch(error => {
-        console.error('Error during download:', error);
-        alert('Error downloading video. Please try again.');
-    });
+    try {
+        await youtubedl(youtubeLink, {
+            'output': videoPath,
+            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best',
+            'postprocessor-args': '-c:v libx264 -preset fast',
+            'merge-output-format': 'mp4',
+            'no-check-certificate': true,
+            'no-warnings': true,
+            'prefer-free-formats': true,
+            'add-header': [
+                'referer:youtube.com',
+                'user-agent:googlebot'
+            ]
+        });
+
+        alert('Download and conversion complete!');
+        // cs.evalScript(`$.runScript.importVideo("${videoPath.replace(/\\/g, '\\\\')}")`);
+    } catch (error) {
+        console.error('Error during download or conversion:', error);
+        alert('Error processing video. Please try again.');
+    }
 }
 
-
-window.downloadVideo = downloadVideo;
+window.downloadAndConvertVideo = downloadAndConvertVideo;
