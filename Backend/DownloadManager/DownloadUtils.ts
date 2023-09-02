673aa542af910a94699e1f98f6b3057982ca81b9
Akki <akshat.kr12090@gmail.com>
2023-09-02 18:16:34 -0400

    add download video and audio only

--- Backend/DownloadManager/DownloadUtils.ts
+++ Backend/DownloadManager/DownloadUtils.ts
@@ -1,30 +1,74 @@
 // DownloadUtils.ts
+import { spawn } from 'child_process';
 
-// Function to check if NVENC is supported and enabled
-const checkNvencSupport = (): boolean => {
-  // Implement NVENC support checking logic here
-  console.log('Checking NVENC support.');
-  return false;  // Placeholder
+const downloadStream = (format: 'video' | 'audio', outputPath: string, link: string): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const ytDlpPath = "C:\\Users\\Akshat Kumar\\AppData\\Roaming\\Python\\Python311\\Scripts\\yt-dlp.exe";
+    const filename = `${outputPath}%(title)s_${format}.%(ext)s`;
+
+    let args: string[];
+    if (format === 'video') {
+      args = ['-f', 'bestvideo[acodec=none]', '-o', filename, link];
+    } else {
+      args = ['-f', 'bestaudio[vcodec=none]', '-o', filename, link];
+    }
+
+    const ytDlp = spawn(ytDlpPath, args);
+
+    let errorData = '';
+    let outputData = '';
+
+    ytDlp.stderr.on('data', (data) => {
+      errorData += data.toString();
+    });
+
+    ytDlp.stdout.on('data', (data) => {
+      outputData += data.toString();
+    });
+
+    ytDlp.on('close', (code) => {
+      if (code !== 0) {
+        console.error("Error output:", errorData);
+        reject(new Error(errorData));
+      } else {
+        // Logic to parse the output will go here
+        resolve(outputData);
+      }
+    });
+  });
 };
 
-// Function to download video
-const downloadVideo = (url: string): void => {
-  // Implement download logic here
-  console.log('Downloading video.');
+const downloadVideo = async (url: string): Promise<void> => {
+  try {
+    const outputPath = 'C:\\Users\\Akshat Kumar\\Editing\\Media\\PProClipFetch\\';
+    const outputData = await downloadStream('video', outputPath, url);
+    console.log("Video downloaded successfully:", outputData);
+  } catch (error) {
+    console.error("Failed to download video:", error);
+  }
 };
 
-// Function to download audio
-const downloadAudio = (url: string): void => {
-  // Implement download logic here
-  console.log('Downloading audio.');
-};
+const downloadAudio = async (url: string): Promise<void> => {
+  try {
+    const outputPath = 'C:\\Users\\Akshat Kumar\\Editing\\Media\\PProClipFetch\\';
+    const outputData = await downloadStream('audio', outputPath, url);
+    console.log("Audio downloaded successfully:", outputData);
+  } catch (error) {
+    console.error("Failed to download audio:", error);
+  }
+}
 
-// Function to download combined audio and video
 const downloadCombined = (url: string): void => {
   // Implement download logic here
   downloadVideo(url);
   downloadAudio(url);
   console.log('Combining audio and video.');
+};
+
+const checkNvencSupport = (): boolean => {
+  // Implement NVENC support checking logic here
+  console.log('Checking NVENC support.');
+  return false;  // Placeholder
 };
 
 // Export the initiateDownload function
